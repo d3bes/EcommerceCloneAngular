e19c6ad5a23dbcfaaeb7dee88b3f08518018406d
n_k00001 <nooorykhalef@gmail.com>
2023-07-23 17:22:12 +0300

    catogry

--- src/app/Component/category/category.component.ts
+++ src/app/Component/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductCategoryDTO } from 'src/app/Models/product-category-dto';
+import { CatogriesService } from 'src/app/Services/catogries.service';
 
 @Component({
   selector: 'app-category',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent {
+  catogries: ProductCategoryDTO[] = [];
 
+  constructor(private catogriesService: CatogriesService) { }
+
+  ngOnInit():void {
+    this.catogriesService.getAllCatogries().subscribe({
+      next: (data: ProductCategoryDTO[]) => {
+        this.catogries = data;
+        console.log(this.catogries);
+      },
+      error: (error: any) => {
+        console.error('Error fetching brands:', error);
+      },
+      complete: () => {
+        console.log('Fetching brands completed.');
+      }
+    });
+  }
 }
--- src/app/Services/catogries.service.ts
+++ src/app/Services/catogries.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductCategoryDTO } from '../Models/product-category-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatogriesService {
 
-  constructor() { }
+  private http={};
+  private apiBaseUrl = 'http://localhost:5216/api';
+  constructor(private httpclient:HttpClient)
+  {
+    // this.http={
+    //   headers:new HttpHeaders({
+    //     'Content-Type': 'application/json',
+    //     'Authorization': 'Access-token'
+    //   })
+    // };
+  }
+  getAllCatogries():Observable<ProductCategoryDTO[]>{
+    return this.httpclient.get<ProductCategoryDTO[]>(`${this.apiBaseUrl}/ProductCategory`);
+  }
 }
