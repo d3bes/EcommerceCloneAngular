10a4802577941e4afe2b6b054a9e8e697ca8554a
Ahmedosama180 <ahmed.osama01289@gmail.com>
2023-07-29 11:57:37 -0700

    chang min slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProducdDetailesComponent } from './Component/producd-detailes/producd-detailes.component';
 import { NavbarComponent } from './Component/navbar/navbar.component';
 import { HomeComponent } from './Component/home/home.component';
+import { SignUpComponent } from './Component/sign-up/sign-up.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo:'home', pathMatch: 'full' },
   {path:'home',component:HomeComponent,title: 'Home'},
+  {path:'signup',component:SignUpComponent,title:'Sign Up'},
+
   { path: 'prd/:productID', component:ProducdDetailesComponent }
 
 ];
