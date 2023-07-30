25cc461777b5282927c025fa572ea002775a6d8e
Rana Mohsen <134969817+rana1369@users.noreply.github.com>
2023-07-30 09:56:44 +0300

    ww

--- src/app/Component/category/category.component.ts
+++ src/app/Component/category/category.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Iproduct } from 'src/app/Models/iproduct';
 import { ProductCategoryDTO } from 'src/app/Models/product-category-dto';
 import { CatogriesService } from 'src/app/Services/catogries.service';
 import { ProductService } from 'src/app/Services/product.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-category',
@@ -12,24 +13,25 @@ import { ProductService } from 'src/app/Services/product.service';
 })
 export class CategoryComponent {
   catId: number = 0;
-  product: Iproduct | undefined = undefined;
+  products: Iproduct[] | undefined = undefined;
 
-  constructor(
+  constructor( private router: Router, 
     private activatedRoute: ActivatedRoute,
     private productsevice: ProductService
   ) {}
+  urlImage:string ="http://localhost:5195/files/images/";
 
   ngOnInit(): void {
     this.catId = this.activatedRoute.snapshot.paramMap.get('categoryID')
       ?Number(this.activatedRoute.snapshot.paramMap.get('categoryID'))
       : 0;
 
-       console.log('Product ID:', this.catId);
+       console.log('category ID:', this.catId);
 
     this.productsevice.getProductsByCategoryID(this.catId).subscribe({
       next: (data) => {
-        this.product = data;
-        console.log('Product:', this.product);
+        this.products = data;
+        console.log('Product:', this.products);
       },
       error: (error) => {
         console.log('Error:', error);
@@ -37,4 +39,21 @@ export class CategoryComponent {
     });
   }
  
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+  
+    },
+    nav: true
+  }
+  
 }
